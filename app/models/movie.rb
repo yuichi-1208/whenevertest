ac0af79c45a5b845d749fc772b0439ac370cd8c0
yuichi oyama <yuichi19991208@gmail.com>
2020-10-24 11:05:16 +0900

    タイムゾーンを日本へ変更し、movieモデルを作成

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ApplicationRecord
+  enum status: {公開前:0, 公開中:1}
+  def self.publish_check
+    Movie.all.each do |movie|
+      if movie.publish_at < Time.now
+        movie.status = 1
+        movie.save
+      end
+    end
+  end
 end
--- db/migrate/20201024012951_create_movies.rb
+++ db/migrate/20201024012951_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
     create_table :movies do |t|
       t.string :title
       t.text :content
-      t.integer :status
+      t.integer :status, default: 0
       t.timestamp :publish_at
 
       t.timestamps
